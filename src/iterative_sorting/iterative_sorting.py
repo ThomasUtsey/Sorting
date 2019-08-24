b7598b196b528058586308926a5433c821cd4b85
Thomas Utsey <tom@edgegluer.com>
2019-08-23 22:36:43 -0400

    Add Selection Sort Python
    Test successful
    
    Add notes.py
    
    Add comments

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,39 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+# TO-DO: Complete the selection_sort() function below
 
 
-
-        # TO-DO: swap
-
-
-
+def selection_sort(arr):
+    # Add a loop with that iterates over the list stopping at second to last
+    # index because the last number will not need to be compared because it
+    # will already have been replaced with the largest.
+    for i in range(0, len(arr)-1):
+        # assign the current index as the min_index to be compared against
+        # with the following indexes in the inner loop
+        min_index = i
+        # add an additional loop to compare agaist min_index it will find the
+        # smaller smallest value if one exist.
+        for j in range(i+1, len(arr)):
+            # the i+1 is important so that we are comparing the min_index t
+            # numbers that follow after i since the indexes before have alread
+            # been resolved
+            if arr[j] < arr[min_index]:
+                min_index = j
+            # compare the current value of index j to the set min_index if its
+            # smaller reassign the value to the current index j
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+            # check to see if the value actually changed if so swap the two
+            # values.
 
     return arr
+    # TO-DO:  implement the Bubble Sort function below
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
---
+++ src/whiteboard.py
@@ -0,0 +1,26 @@
+'''We'll say that a positive int divides itself 
+if every digit in the number divides into the 
+number evenly. So for example 128 divides 
+itself since 1, 2, and 8 all divide into 128 
+evenly. We'll say that 0 does not divide into 
+anything evenly, so no number with a 0 digit divides itself. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+'''
+
+
+# def dividesSelf(numbers):
+#     numbers = str(numbers)
+#     for num in num:
+#         if int(numbers) == 0:
+#             return(False)
+#         if int(num) % int(numbers) == 1:
+#             return (False)
+#         else:
+#             return(False)
+#     return (True)
+
+
+# dividesSelf(128)
+# dividesSelf(12)
+# dividesSelf(120)
+
+help(int)
